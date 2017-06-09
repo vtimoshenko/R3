d92e565b0216f3a15b0df43242ff7accde001e7f
vtimoshenko <vtimoshenko@pktbcki.org.rzd>
2017-06-09 15:23:08 +0300

    initial structure

---
+++ rainbow/src/main/java/rzd/pktb/rainbow/core/ApplicationController.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package rzd.pktb.rainbow.core;
+
+/**
+ *
+ * @author vtimoshenko
+ */
+public class ApplicationController {
+    
+}
